89f7b0701e4fa3bc20a01507a8266df23c65c398
Muhammadali <boburjonr934@gmail.com>
2025-09-18 14:55:42 +0500

    push

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,69 @@
-import { useState, useEffect } from "react"
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { useParams } from "react-router-dom";
-import axios from "axios"
 
+type Product = {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+}
 
 export default function Home() {
+    const { id } = useParams<{id:string}>();
 
-    const [product, setProduct] = useState({});
-    const [loader, setLoader] = useState(true);
+    if(!id){
+        return <p>id yoq ekan</p>
+    }
 
-    const { id } = useParams()
+    const getProduct = async (id: string) => {
+        let { data } = await axios.get<Product>(`https://jsonplaceholder.typicode.com/comments/${id}`)
+        return data;
+    };
 
-    useEffect(() => {
-        const Api = async () => {
-            try {
-                setLoader(true)
-                const res = await axios.get(`https://jsonplaceholder.typicode.com/comments/${id}`)
-                setProduct(res.data);
-            } catch (error) {
-                console.log(error);
-            } finally {
-                setLoader(false);
-            }
-        }
+    const {data, isPending} = useQuery({
+        queryKey: [id],
+        queryFn: () => getProduct(id),
+    })
 
-        Api()
-    }, [id]);
+    console.log(data)
 
-    if (loader) {
-        return <h2>Loading ...</h2>
+    // const { id } = useParams();
+    // const [product, setProduct] = useState<Product | null>(null);
+    // const [loader, setLoader] = useState(true);
+
+    // useEffect(() => {
+    //     const Api = async () => {
+    //         try {
+    //             setLoader(true)
+    //             const res = await axios.get(`https://jsonplaceholder.typicode.com/comments/${id}`)
+    //             setProduct(res.data);
+    //         } catch (error) {
+    //             console.log(error);
+    //         } finally {
+    //             setLoader(false);
+    //         }
+    //     }
+
+    //     Api()
+    // }, [id]);
+
+    if(isPending){
+        return (
+            <p>loading ...</p>
+        )
+    }
+
+    if(!data){
+        return (
+            <p>data yoq</p>
+        )
     }
 
     return (
         <div>
-            <p>{product.body}</p>
+            <p>{data.name}</p>
         </div>
     )
 }
