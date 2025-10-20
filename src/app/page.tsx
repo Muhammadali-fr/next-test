2e12df1f882cbff1d23ac32567ed446f2c6f67f9
Muhammadali <boburjonr934@gmail.com>
2025-10-20 10:17:09 +0500

    fi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client'
+import { useState } from 'react';
 
 export default function Page() {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+
+  console.log('page rerendered')
   return (
-    <div>app</div>
-  )
-}
+    <div>
+      <input placeholder='name' className='border' type="text" value={name} onChange={e => setName(e.target.value)} />
+    </div>
+  );
+};
