9e3952e1806f5220fba96bc472403fe0b7881a2e
Muhammadali Jamolov <boburjonr934@gmail.com>
2026-02-10 17:54:19 +0500

    caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-// "use client";
-export default async function HomePage() {
-
-  // (async function () {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'force-cache' }).then((response) => response.json());
-    console.log(res);
-  // })();
+export default function page(){
   return (
-    <section>
-      {res.map((post: any) => (
-        <article key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </article>
-      ))}
-    </section>
-  );
+    <div>home page</div>
+  )
 }
